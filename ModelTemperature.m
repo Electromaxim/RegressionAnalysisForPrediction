10946155206b32cfe8a49b7fda02013ecb902345
Electromaxim <electromaxim@gmail.com>
2016-06-22 17:43:04 +0300

    Update ModelTemperature.m

--- ModelTemperature.m
+++ ModelTemperature.m
@@ -5,11 +5,13 @@ drybulb0 = drybulb - m;
 % Fit double-sine model
 model = fit(dates, drybulb0, 'sin2')
 
+%% Visualize Model Accuracy
 pred = model(dates) + m;
 res = drybulb - pred;
 fitPlot(dates, [drybulb pred], res);
 disp(['Mean Absolute Error: ' num2str(mean(abs(res))) ' degrees F']);
 
+% Analyze Serial Correlation in Residuals
 figure;
 subplot(2,1,1);
 autocorr(res,50);
@@ -17,12 +19,14 @@ title('Serial Correlation of Stochastic series');
 subplot(2,1,2);
 parcorr(res(1:1000),50);
 
+% Modeling the Stochastic Component with a seasonal AR model
 lags = [1 2 3 4 23 24 25 47 48 49];
 Xres = lagmatrix(res, lags);
 [beta, betaci, res2] = regress(res, Xres);
 disp('Lags Coefficients and Confidence Intervals');
 disp([lags' beta betaci])
 
+% Analyze Residuals of Regression for Serial Correlation
 figure;
 subplot(2,1,1);
 plot(dates, res2); datetick
@@ -30,11 +34,23 @@ title('Regression Residuals & Their Serial Correlation');
 subplot(2,1,2);
 autocorr(res2(lags(end)+1:end),50);
 
+%% Fit a Distribution to Residuals
 PD = fitdist(res2, 'tlocationscale');
+
+%% Summary of model
+% The temperature model can now be defined by,
+% 
+% * The mean temperature "m"
+% * The sinusoidal model "model"
+% * Regression parameters "beta"
+% * Autocorrelation lags "lags"
+% * The residual probability distribution "PD"
+% * Optional presample data (the last observations of temperature for regression)
 tempModel = struct('m', m, 'sinmodel', model, 'reglags', lags, 'regbeta', beta, 'dist', PD, 'presample', res(end-lags(end)+1:end));
 save SavedModels\TemperatureModel.mat -struct tempModel
 clearvars -except tempModel dates drybulb
 
+% Simulate our model
 newDates = dates(end-365*24+1:end);
 simTemp = simulateTemperature(tempModel, newDates, 1);
 
